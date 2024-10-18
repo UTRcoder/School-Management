78438320ffb00f283d85c2a9facdabcdee0c40fa
UTRcoder <hackerutr0102@gmail.com>
2024-10-18 07:24:52 +0530

    ok

---
+++ src/app/(dashboard)/list/messages/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react';
+
+const App = () => {
+    
+    return (
+        <div className='bg-red-300 rounded-lg w-auto h-auto m-auto align-middle' >
+            <div className='m-auto p-4'>Requires Premium<br/> Kindly Contact at <a className='underline text-white' href="https://www.skoolu.com" target="_blank">www.skoolu.com</a></div>
+        </div>
+    );
+}
+
+export default App;
