1f63672126965e5394aa6fe542109996332dbfc1
UTRcoder <hackerutr0102@gmail.com>
2024-11-26 20:59:48 +0530

    done

--- src/app/(dashboard)/list/messages/page.tsx
+++ src/app/(dashboard)/list/messages/page.tsx
@@ -5,7 +5,7 @@ const App = () => {
     
     return (
         <div className='bg-red-300 rounded-lg w-auto h-auto m-auto align-middle' >
-            <div className='m-auto p-4'>Requires Premium<br/> Kindly Contact at <a className='underline text-white' href="https://www.skoolu.com" target="_blank">www.skoolu.com</a></div>
+            <div className='m-auto p-4'>Requires Premium<br/> Kindly Contact at <a className='underline text-white' href='www.skoolu.com' target='blank'>www.skoolu.com</a></div>
         </div>
     );
 }
